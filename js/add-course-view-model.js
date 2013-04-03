8024f79fb46693d3243843f9dd3a4180217bae5c
Albert Villaroman <avdeveloper@gmail.com>
2013-04-03 03:19:00 -0400

    refined the interface including view courses component

---
+++ js/add-course-view-model.js
@@ -0,0 +1,83 @@
+function AddCourseViewModel() {
+
+  /* Overview
+   * ~Private Members
+   * ~Private Methods
+   * ~Constructor
+   * ~Public Members
+   * ~Public Methods
+   * ---------------------------------- */
+
+  /* ~Private Members
+   * ---------------------------------- */
+  var self = this,
+      Course = Parse.Object.extend('Course'),
+      CourseQuery = new Parse.Query(Course);
+
+  /* ~Private Methods
+   * ---------------------------------- */
+  var pushCourses = function (courses) {
+    courses.forEach(pushCourse);
+  };
+
+  var pushCourse = function (course) {
+    self.courses.unshift(course.attributes);
+    resetFormData();
+  };
+
+  var showError = function (object, error) {
+    console.error('Encountered an error: ' + error);
+  };
+
+  var resetFormData = function () {
+    self.courseNumber('');
+    self.credits('');
+    self.description('');
+  };
+
+  var loadCourses = {
+    success: pushCourses,
+    error: showError
+  };
+
+  /**
+   * ~Constructor
+   * ---------------------------------- */
+  (function initialize() {
+    Parse.initialize('U9fUNVz6nQHRDZqIM3DaR9ydC3Vb6pMWhq5eyj60', 'eZ79KBnlJ3siGkndjM1LOsmcOsBhIAJXAXOd8rf1');
+    CourseQuery.find(loadCourses);
+  })();
+
+
+  /**
+   * ~Public Members
+   * ---------------------------------- */
+  self.department = ko.observable();
+  self.initials = ko.observable();
+  self.courseNumber = ko.observable();
+  self.credits = ko.observable();
+  self.name = ko.observable();
+  self.description = ko.observable();
+  self.courses = ko.observableArray();
+  self.formData = ko.computed(function() {
+    return {
+      department: self.department(),
+      initials: self.initials(),
+      courseNumber: self.courseNumber(),
+      credits: self.credits(),
+      name: self.name(),
+      description: self.description()
+    };
+  }, this);
+
+  /*
+   * ~Public Methods
+   * ---------------------------------- */
+  self.saveCourse = function (data, event) {
+    new Course().save(self.formData(), {
+      success: pushCourse,
+      error: showError
+    });
+  };
+
+}
--- main.js
+++ main.js
@@ -1,48 +1,24 @@
 $(function () {
-  Parse.initialize('U9fUNVz6nQHRDZqIM3DaR9ydC3Vb6pMWhq5eyj60', 'eZ79KBnlJ3siGkndjM1LOsmcOsBhIAJXAXOd8rf1');
-  var Course = Parse.Object.extend('Course');
+  /**
+   * Overview
+   * ~jQuery bindings
+   * ~Knockout bindings
+   * ---------------------------------- */
 
-  function AddCourseViewModel() {
-    var self = this;
+  /* ~jQuery bindings
+   * ---------------------------------- */
+  (function initializejQueryBindings () {
+    var form = $('form'),
+        newCourseButton = $('header > button'),
+        addCourseButton = $('form > input[type=submit]');
 
-    // Set data
-    self.department = ko.observable();
-    self.initials = ko.observable();
-    self.courseNumber = ko.observable();
-    self.credits = ko.observable();
-    self.description = ko.observable();
-    self.formData = ko.computed(function() {
-      return {
-        department: self.department(),
-        initials: self.initials(),
-        courseNumber: self.courseNumber(),
-        credits: self.credits(),
-        description: self.description()
-      };
-    }, this);
+    form.hide();
+    newCourseButton.click(function (event) {
+      form.is(':visible') ? form.hide(200) : form.show(200);
+    });
+  })();
 
-    // Utils
-    self.resetFormData = function () {
-      self.courseNumber('');
-      self.credits('');
-      self.description('');
-    };
-    
-    // Handle events
-    self.saveCourse = function (data, event) {
-      // Save course to database
-      var course = new Course();
-      course.save(self.formData(), {
-        success: function (data) {
-          console.log("Course has been saved!");
-          self.resetFormData();
-        },
-        error: function (data, error) {
-          console.error("Could not save course: " + error);
-        }
-      });
-    };
-  }
-
-  ko.applyBindings(new AddCourseViewModel());
+  /* ~Knockout bindings
+   * ---------------------------------- */
+  ko.applyBindings(new AddCourseViewModel);
 });
